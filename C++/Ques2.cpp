fe4e76c4c260aaa9f371a153fa75aae865f69351
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-01 23:18:34 +0530

    Create Ques2.cpp

---
+++ C++/Ques2.cpp
@@ -0,0 +1,20 @@
+/* Write a function that takes the binary representation of a positive integer and returns the number of
+set bits it has (also known as the Hamming weight).*/
+
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            if (n & 1)
+            {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+};
