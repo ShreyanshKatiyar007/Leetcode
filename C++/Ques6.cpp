e61478b0f89c8fde23da0c96166eee073ada0fe8
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-02 23:41:11 +0530

    Create Ques6.cpp

---
+++ C++/Ques6.cpp
@@ -0,0 +1,31 @@
+/* You are given an integer mountain array 'arr' of length 'n' where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.*/
+
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int s = 0;
+        int e = arr.size() - 1;
+
+        int mid = s + (e - s) / 2;
+
+        while (s < e)
+        {
+            if (arr[mid] < arr[mid + 1])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return s;
+    }
+};
