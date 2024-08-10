d956b31deb242670a8766940e44104be3ab2ba8b
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-10 23:46:02 +0530

    Create Ques9.cpp

---
+++ C++/Ques9.cpp
@@ -0,0 +1,24 @@
+/* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.*/
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
